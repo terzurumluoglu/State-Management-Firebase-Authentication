69951cb1d45d2202c6c1bdef1ba8c26eabb1f6d6
terzurumluoglu <toprakerzurumluoglu@hotmail.com>
2020-11-04 13:27:57 +0300

    Firebase Module created and used at AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './_components/home/home.module';
 
+import { FirebaseModule } from "./_module";
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,9 @@ import { HomeModule } from './_components/home/home.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeModule
+    HomeModule,
+
+    FirebaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
