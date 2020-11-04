efc6ed8b662f28a501b39f27ec57e1cb9cda6dca
terzurumluoglu <toprakerzurumluoglu@hotmail.com>
2020-11-04 17:16:18 +0300

    Added Observable AuthState and edited signIn

--- src/app/_services/auth/auth.service.ts
+++ src/app/_services/auth/auth.service.ts
@@ -1,23 +1,28 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from "@angular/fire/auth";
+import { Store } from '@ngrx/store';
 import * as firebase from "firebase/app";
 import 'firebase/auth';
-import { from } from 'rxjs';
+import { from, Observable } from 'rxjs';
 import { providers } from 'src/app/_model/signIn';
+import { AppState, AuthState } from 'src/app/_module/state/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  authState$: Observable<AuthState> = this.store.select(p => p.authState);
+
   constructor(
-    private _fireAuth: AngularFireAuth
+    private _fireAuth: AngularFireAuth,
+    private store: Store<AppState>
   ) { }
 
   SignIn(type: string) {
     let provider = providers.find(p => p.type.name === type);
     provider.type.provider.addScope(provider.type.scope);
-    return from(this._fireAuth.signInWithPopup(provider.type.provider);
+    return from(this._fireAuth.signInWithPopup(provider.type.provider));
   }
 
   SignOut() {
