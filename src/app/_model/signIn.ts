e7876e173547ea4c6123c6d4bfff4051feb85d9e
terzurumluoglu <toprakerzurumluoglu@hotmail.com>
2020-11-04 13:55:51 +0300

    Update signIn.ts

--- src/app/_model/signIn.ts
+++ src/app/_model/signIn.ts
@@ -1,3 +1,6 @@
+import * as firebase from "firebase/app";
+import 'firebase/auth';
+
 enum LoginType {
     google = 'google',
     facebook = 'facebook',
@@ -16,5 +19,39 @@ export const LOGINTYPES = [
     {
         title: LoginType.github,
         cls: 'btn btn-dark'
+    }
+];
+
+export class Provider {
+    type: Login;
+}
+
+export class Login {
+    name: string;
+    provider: any;
+    scope: string
+}
+
+export const providers: Provider[] = [
+    {
+        type: {
+            name: LoginType.google,
+            provider: new firebase.default.auth.GoogleAuthProvider(),
+            scope: 'https://www.googleapis.com/auth/userinfo.profile'
+        }
+    },
+    {
+        type: {
+            name: LoginType.facebook,
+            provider: new firebase.default.auth.FacebookAuthProvider(),
+            scope: 'public_profile,email'
+        }
+    },
+    {
+        type: {
+            name: LoginType.github,
+            provider: new firebase.default.auth.GithubAuthProvider(),
+            scope: 'user'
+        }
     }
 ];
