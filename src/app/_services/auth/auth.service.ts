be1a57943463d3115c046eaf7e3de2d49f1b43dd
terzurumluoglu <toprakerzurumluoglu@hotmail.com>
2020-11-04 13:58:00 +0300

    SignIn and SignOut methods

--- src/app/_services/auth/auth.service.ts
+++ src/app/_services/auth/auth.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from "@angular/fire/auth";
+import * as firebase from "firebase/app";
+import 'firebase/auth';
+import { providers } from 'src/app/_model/signIn';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private _fireAuth: AngularFireAuth
+  ) { }
+
+  SignIn(type: string) {
+    let provider = providers.find(p => p.type.name === type);
+    provider.type.provider.addScope(provider.type.scope);
+    return this._fireAuth.signInWithPopup(provider.type.provider);
+  }
+
+  SignOut() {
+    return this._fireAuth.signOut();
+  }
 }
